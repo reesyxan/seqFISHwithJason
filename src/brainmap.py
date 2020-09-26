ab757198989988e07d13b893623fa5611a420d1a
Jason Fan <jason.fan.74@gmail.com>
2020-09-25 21:02:10 -0400

    expeeriments by name

--- src/brainmap.py
+++ src/brainmap.py
@@ -3,14 +3,19 @@ import requests
 from enum import Enum
 
 class Model(Enum):
+    
     ORGANISM = 'Organism'
     GENE = 'Gene'
     Chromosome = 'Chromosome'
     STRUCTURE = 'Structure'
 
 class BrainMapAPI(object):
+    '''
+    http://help.brain-map.org/display/api/RMA+Path+Syntax
+    '''
     __url_base = 'http://api.brain-map.org/api/v2/data/'
     __url_prototype = path.join(__url_base, '{model}', '{model_id}.json')
+    __query_url = path.join(__url_base, 'query.json')
     __status_url = 'http://api.brain-map.org/api/v2/status/query.json'
 
     @classmethod
@@ -25,8 +30,24 @@ class BrainMapAPI(object):
         r = requests.get(url)
         print(url)
         return r.json()
-    
 
+    @classmethod 
+    def experiments_by_gene(cls, gene):
+        rma_path = ''
+
+        rma_path  = 'model::SectionDataSet,'
+        rma_path += "rma::criteria,[failed$eq'false'],"
+        rma_path += "products[abbreviation$eq'Mouse'],"
+        rma_path += "plane_of_section[name$eq'sagittal'],"
+        rma_path += "genes[acronym$eq'{}']".format(gene)
+
+        url = cls.__query_url + '?criteria='
+        url += rma_path
+        r = requests.get(url)
+        print(r.json())
+
+        
 
 
 print(BrainMapAPI.gene(2))
+BrainMapAPI.experiments_by_gene('Star')
